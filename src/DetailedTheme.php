df847568e39aebe9d41ffcf33ae93e03c9379e7f
Andrii Vasyliev <sol@hiqdev.com>
2017-03-07 17:34:43 +0000

    csfixed

--- src/DetailedTheme.php
+++ src/DetailedTheme.php
@@ -1,13 +1,14 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: tofid
- * Date: 26.01.17
- * Time: 17:27
+ * Flat Theme for hiqdev/yii2-thememanager
+ *
+ * @link      https://github.com/hiqdev/yii2-theme-flat
+ * @package   yii2-theme-flat
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
  */
 
 namespace hiqdev\themes\flat;
-
 
 class DetailedTheme extends \hiqdev\thememanager\DetailedTheme
 {
--- src/widgets/Menu.php
+++ src/widgets/Menu.php
@@ -1,12 +1,11 @@
 <?php
-
-/*
+/**
  * Flat Theme for hiqdev/yii2-thememanager
  *
  * @link      https://github.com/hiqdev/yii2-theme-flat
  * @package   yii2-theme-flat
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
  */
 
 namespace hiqdev\themes\flat\widgets;
