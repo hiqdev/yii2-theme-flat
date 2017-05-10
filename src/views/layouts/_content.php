1ebdcb44b0227a13479fae66c489dbc5174e8ad3
Andrii Vasyliev <sol@hiqdev.com>
2017-05-10 16:46:16 +0000

    added div.container in _content view

--- src/views/layouts/_content.php
+++ src/views/layouts/_content.php
@@ -1,7 +1,9 @@
 <?= \hiqdev\thememanager\widgets\Flashes::widget() ?>
 
 <?php if (Yii::$app->themeManager->isHomePage()) : ?>
-    <?= $content ?>
+    <div class="container">
+        <?= $content ?>
+    </div>
 <?php else: ?>
     <section id="<?= isset($this->params['contentId']) ? $this->params['contentId'] : 'content' ?>" class="container">
         <?= $content ?>
